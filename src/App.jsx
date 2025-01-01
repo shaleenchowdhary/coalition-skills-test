7f96275b7b4859ca39d8919ab610b71e8cd83d23
Shaleen Chowdhary <137061973+shaleenchowdhary@users.noreply.github.com>
2025-01-01 18:43:52 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ export default function App() {
   return (
     <div id="app-container">
       <Navbar />
-      {/* <MainContent patients={patients} /> */}
+      <MainContent patients={patients} />
     </div>
   );
 }
